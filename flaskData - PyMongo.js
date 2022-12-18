66beb0702de764c7505405d89d00d22047de9752
acuerdoskilling <siva.smoorthy@gmail.com>
2022-12-18 17:19:25 +0530

    adds tabular layout in js to format output coming from python-mongo

--- flaskData - PyMongo.js
+++ flaskData - PyMongo.js
@@ -2,41 +2,38 @@
 const button = document.getElementById("theButton")
 const data = document.getElementById("info")
 
-// Create an array of cars to send to the server
-
-const languages = [
-      { "lang":"Swedish", "origin":"Sweden" },
-      { "lang":"Tamil", "origin":"India" },
-      { "lang":"Finnish","origin": "Finland" }
-     ];
-
 // Create an event listener on the button element
 button.onclick= function(){
 
-// Get the reciever endpoint from Python using fetch
-  fetch("http://127.0.0.1:5000/receiver", 
-    {
-      mode: "cors",
-      //credentials: 'include',
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-        'Accept': 'application/json',
-        'Origin': 'http://127.0.0.1:5000/receiver'
-      },
-// Strigify the payload into JSON
-    body:JSON.stringify(languages)}).then(res=>{
-        if(res.ok){
-          return res.json()
-        }else{
-          alert("something is wrong")
-        }
-      }).then(jsonResponse=>{
-        
-// Iterate through the data with Map and write your rendering logic
-//        [jsonResponse].map(Main=> 
-        jsonResponse.map(Main=> 
-   Main._id==="Energy"? data.innerHTML +="<p>"+ Main._id+" "+" is a super-domain to invest":
-   data.innerHTML +="<p>"+ Main._id+" "+"is a good domain to invest" ) 
-} 
-).catch((err) => console.error(err)); } 
+    fetch("http://127.0.0.1:5000/receiver", 
+        {
+          mode: "cors",
+          //credentials: 'include',
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json',
+            'Accept': 'application/json',
+            'Origin': 'http://127.0.0.1:5000/receiver'
+          },
+    // Strigify the payload into JSON
+        body:JSON.stringify(null)})
+    .then(function (response) {
+        return response.json()
+    }).then(function (text) {
+        //console.log('GET response text:')
+        var keys = []
+        document.write("<table border==\"1\"><tr>")
+        for (key in text[0]) {
+            document.write('<td>' + key + '</td>')
+        }
+        document.write("</tr>")
+        for (var i = 0; i < text.length; i++) {
+            document.write('<tr>')
+            for (key in text[i]) {
+                document.write('<td>' + text[i][key] + '</td>')
+            }
+            document.write('</tr>')
+        }
+        document.write("</table>")
+    })
+}
